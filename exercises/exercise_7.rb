949ae510231ee71cd24e3635bc0d9f54c27f1194
Tom Fream <tomfream@gmail.com>
2021-05-26 17:16:32 -0600

    Exercise 7

--- exercises/exercise_7.rb
+++ exercises/exercise_7.rb
@@ -10,3 +10,18 @@ puts "Exercise 7"
 puts "----------"
 
 # Your code goes here ...
+class Employee
+  validates :first_name, :last_name, presence: true
+  validates :hourly_rate, numericality: { only_integer: true, greater_than: 39, less_than: 201 } 
+  validates :store_id, presence: true
+end
+
+class Store 
+  validates :name, length: { minimum: 3 }
+  validates :annual_revenue, numericality: { only_integer: true, greater_than: -1 }
+end
+
+puts "Gimme a new store name: "
+new_store = gets.chomp
+@invalid = Store.create(name: "#{new_store}")
+puts @invalid.errors.full_messages
